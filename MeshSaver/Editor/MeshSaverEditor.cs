00cc0fe6faa90f362e2527c52c83fe52831659e3
pharan <johndygit@live.com.ph>
2015-04-20 02:05:26 +0800

    Removed the need for a MonoBehaviour. Just open up the context menu by right-clicking on a MeshFilter's component label or clicking on its gear icon.

--- MeshSaver/Editor/MeshSaverEditor.cs
+++ MeshSaver/Editor/MeshSaverEditor.cs
@@ -1,51 +1,36 @@
-﻿using UnityEditor;
+using UnityEditor;
 
 using UnityEngine;
 using System.Collections;
 using System.Collections.Generic;
 
-[CustomEditor(typeof(MeshSaver))]
-public class MeshSaverEditor : Editor {
+public static class MeshSaverEditor {
 
-	bool makeNewInstance = true;
-	bool optimizeMesh = false;
-
-	public override void OnInspectorGUI ()
-	{
-		EditorGUILayout.BeginHorizontal();
-		EditorGUILayout.Space();
-		optimizeMesh = EditorGUILayout.ToggleLeft("Optimize Mesh", optimizeMesh);
-		EditorGUILayout.Space();
-		EditorGUILayout.EndHorizontal();
-
-		EditorGUILayout.BeginHorizontal();
-		EditorGUILayout.Space();
-		makeNewInstance = EditorGUILayout.ToggleLeft("Save as new Instance", makeNewInstance);
-		EditorGUILayout.Space();
-		EditorGUILayout.EndHorizontal();
-
-		// Save CurrentMesh button
-		//
-		GUILayout.BeginHorizontal();
-		GUILayout.FlexibleSpace();
-
-		if ( GUILayout.Button("Save Mesh Asset", GUILayout.MaxWidth(200f), GUILayout.MinWidth(150f)) ) {
-			MeshSaver ms = target as MeshSaver;
-			Mesh mesh = ms.GetComponent<MeshFilter>().sharedMesh;
-			SaveMesh(mesh, mesh.name, makeNewInstance, optimizeMesh);
-		}
-
-		GUILayout.FlexibleSpace();
-		GUILayout.EndHorizontal();
+	[MenuItem("CONTEXT/MeshFilter/Save Mesh...")]
+	public static void SaveMeshInPlace (MenuCommand menuCommand) {
+		MeshFilter mf = menuCommand.context as MeshFilter;
+		Mesh m = mf.sharedMesh;
+		SaveMesh(m, m.name, false, true);
 	}
 
-	public static void SaveMesh(Mesh mesh, string name, bool makeNewInstance, bool optimizeMesh) {
+	[MenuItem("CONTEXT/MeshFilter/Save Mesh As New Instance...")]
+	public static void SaveMeshNewInstanceItem (MenuCommand menuCommand) {
+		MeshFilter mf = menuCommand.context as MeshFilter;
+		Mesh m = mf.sharedMesh;
+		SaveMesh(m, m.name, true, true);
+	}
+
+	public static void SaveMesh (Mesh mesh, string name, bool makeNewInstance, bool optimizeMesh) {
 		string path = EditorUtility.SaveFilePanel("Save Separate Mesh Asset", "Assets/", name, "asset");
-		if(string.IsNullOrEmpty(path)) return;
+		if (string.IsNullOrEmpty(path)) return;
+        
 		path = FileUtil.GetProjectRelativePath(path);
 
-		Mesh meshToSave = (makeNewInstance) ? Instantiate(mesh) as Mesh : mesh;
-		if(optimizeMesh) meshToSave.Optimize();
+		Mesh meshToSave = (makeNewInstance) ? Object.Instantiate(mesh) as Mesh : mesh;
+		
+        if (optimizeMesh)
+            meshToSave.Optimize();
+        
 		AssetDatabase.CreateAsset(meshToSave, path);
 		AssetDatabase.SaveAssets();
 	}
--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # Unity-MeshSaver
-A C# Editor Script (with attachable MonoBehaviour) that allows you to save a usable Mesh .asset file into your project.
+A C# Editor Script that allows you to save a usable Mesh .asset file into your project.
+
+# How to Use:
+Right click on a MeshFilter component's label or click on its gear icon to open up its context menu. Then select either "Save Mesh..." or "Save Mesh As New Instance...". Note that "Save Mesh..." will not work if the mesh comes from an imported 3D asset/prefab.
